7c464b50507a52ffbf26d62e542215a0725ffb5c
Leivysallia <85811527+Leivysallia@users.noreply.github.com>
2022-04-26 11:26:50 -0400

    Update poker.sh
    removed debug

--- poker.sh
+++ poker.sh
@@ -227,8 +227,8 @@ echo "$flag"
 
 ## DEBUG
 
-echo "$flush" "$pair1" "$pair2" "$pair3" "$pair4" "$three1" "$three2" "$three3" "$straight" "$four1" "$four2" | fmt -1 > debug.test
+##	echo "$flush" "$pair1" "$pair2" "$pair3" "$pair4" "$three1" "$three2" "$three3" "$straight" "$four1" "$four2" | fmt -1 > debug.test
 
 ## END DEBUG
 
-##	clean
+clean
