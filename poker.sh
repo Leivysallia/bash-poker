27b2a2aad26dea6e86473c038a36e68cf2337250
Leivysallia <aelenydd@gmail.com>
2022-09-01 08:17:40 -0400

    Major Update - Files > Arrays

--- poker.sh
+++ poker.sh
@@ -1,230 +1,43 @@
 #!/bin/bash
 
-source functions
-##	source vars
+clear
+
+source functions.bash
+source vars.bash
 
 flag="HIGH"
 
-start
-ruffle
+iter=51
+IFS=$OLDIFS
+
+until [[ $iter -lt 0 ]]
+do
+pull=$(( RANDOM % 52 ))
+codex[$pull]="${index[$pull]}"
+
+if [[ ${index[$pull]} -ne 0 ]]
+then
+	index[$pull]=$'0'
+fi
+if [[ ${codex[$pull]} -ne 0 ]]
+then
+iteration[$iter]=${library[$pull]}
+echo ${library[$pull]} 
+codex[$pull]=$'0'
+iter=$(( iter - 1 ))
+else
+	RANDOM=$RANDOM
+pull=$(( RANDOM % 52 ))
+fi
+done > test.bash
 
 deal
 
-phase
+render
+##iswin
 
 mull
 
-read -n1 -r -p $'How Many to Discard? [0-3]:\n' dis
-while [[ dis -gt 3 || dis -lt 0 ]]; do
-	echo $'Invalid Input:\n'
-	read -n1 -r -p $'How Many to Discard? [0-3]:\n' dis
-done
+render
+##iswin
 
-if [[ dis -eq 1 ]]; then
-	read -n1 -r -p $'\n:Index Number [1-5]:\n' one
-	sort -n hand |head -"$one" |tail -1 >> graveyard
-fi
-
-if [[ dis -eq 2 ]]; then
-	read -n1 -r -p $'\n:Index Number [1-5]:\n' one
-	read -n1 -r -p $'\n:Index Number [1-5]:\n' two
-	while [[ $two -eq $one ]]; do
-		read -n1 -r -p $'\n:Invalid, Select. [1-5]:\n' two
-	done
-	sort -n hand |head -"$one" |tail -1 >> graveyard
-	sort -n hand |head -"$two" |tail -1 >> graveyard
-fi
-
-if [[ dis -eq 3 ]]; then
-	read -n1 -r -p $'\n:Index Number [1-5]:\n' one
-	read -n1 -r -p $'\n:Index Number [1-5]:\n' two
-		while [[ $two -eq $one ]]; do
-		read -n1 -r -p $'\n:Invalid, Select. [1-5]:\n' two
-		done
-	read -n1 -r -p $'\n:Index Number [1-5]:\n' three
-		while [[ $three -eq $one || $three -eq $two ]]; do
-		read -n1 -r -p $'\n:Invalid, Select. [1-5]:\n' three
-		done
-	sort -n hand |head -"$one" |tail -1 >> graveyard
-	sort -n hand |head -"$two" |tail -1 >> graveyard
-	sort -n hand |head -"$three" |tail -1 >> graveyard
-fi
-
-
-
-
-ruffle
-
-cat hand >> graveyard
-
-ruffle
-
-uniq -u graveyard > hand 
-
-ruffle
-
-
-seed=$(cat hand |wc -l)
-seed=$((5 - seed))
-
-
-sort -Ru library | head -$seed >> hand
-
-
-cat hand > discard
-cat library >> discard
-ruffle
-uniq -u discard > library
-cat /dev/null > discard
-ruffle
-
-echo $'\n'
-
-mull
-
-ruffle
-
-calc
-
-## FLUSH CODE
-
-if [[ "$flush" -eq 1 ]]; then
-	flush=1
-else
-	flush=0
-fi
-
-## END FLUSH CODE
-
-## PAIR CODE
-
-if [[ "$pair1" -eq 1 ]] ; then
-    flag=Pair
-
-fi
-
-if [[ "$pair2" -eq 1 ]] ; then
-    flag=Pair
-
-fi
-
-if [[ "$pair3" -eq 1 ]] ; then
-    flag=Pair
-
-fi
-
-if [[ "$pair4" -eq 1 ]] ; then
-    flag=Pair
-
-fi
-## END PAIR CODE
-
-## TWO PAIR CODE
-if [[ "$pair1" -eq 1 ]]; then
-	if [[ "$pair2" -eq 1 ]]; then
-	flag="Two Pair"
-	fi
-fi
-if [[ "$pair1" -eq 1 ]]; then
-	if [[ "$pair3" -eq 1 ]]; then
-	flag="Two Pair"
-	fi
-fi
-if [[ "$pair1" -eq 1 ]]; then
-	if [[ "$pair4" -eq 1 ]]; then
-	flag="Two Pair"
-	fi
-fi
-if [[ "$pair2" -eq 1 ]]; then
-	if [[ "$pair3" -eq 1 ]]; then
-	flag="Two Pair"
-	fi
-fi
-if [[ "$pair2" -eq 1 ]]; then
-	if [[ "$pair4" -eq 1 ]]; then
-	flag="Two Pair"
-	fi
-fi
-if [[ "$pair3" -eq 1 ]]; then
-	if [[ "$pair4" -eq 1 ]]; then
-	flag="Two Pair"
-	fi
-fi
-## END TWO PAIR CODE
-
-## THREE OF A KIND CODE
-
-if [[ "$three1" -eq 1 ]]; then
-    flag="Three of a Kind"
- 
-fi
-
-if [[ "$three2" -eq 1 ]]; then
-    flag="Three of a Kind" 
-   
-fi
-
-if [[ "$three3" -eq 1 ]]; then
-    flag="Three of a Kind"
-
-fi
-## END THREE OF A KIND CODE
-
-## STRAIGHT CODE
-
-
-if [[ "$straight" -eq 1 ]]; then
-	straight=1
-fi
-## END STRAIGHT CODE
-
-## FULL HOUSE CODE
-if [[ "$three1" -eq 1 ]]; then
-	if [[ "$pair4" -eq 1 ]]; then
-	flag="Full House"
-	fi
-fi
-if [[ "$three3" -eq 1 ]]; then
-	if [[ "$pair1" -eq 1 ]]; then
-	flag="Full House"
-	fi
-fi
-## END FULL HOUSE CODE
-
-## FOUR OF A KIND CODE
-if [[ "$four1" -eq 1 ]] ; then
-    flag="Four of a Kind"
-
-fi
-
-if [[ "$four2" -eq 1 ]] ; then
-    flag="Four of a Kind"
-
-fi
-## END FOUR OF A KIND CODE
-
-## STRAIGHT FLUSH CODE
-if [[ "$flush" -eq 1 ]]; then
-	if [[ "$straight" -eq 1 ]]; then
-	flag="Straight Flush"
-	fi
-fi
-## END STRAIGHT FLUSH CODE
-
-echo $'\n'
-echo "$flag"
-
-## DEBUG
-
-##	echo "$flush" "$pair1" "$pair2" "$pair3" "$pair4" "$three1" "$three2" "$three3" "$straight" "$four1" "$four2" | fmt -1 > debug.test
-
-## END DEBUG
-
-## DEBUG
-##	calc_files
-##	echo "$flush" "$pair1" "$pair2" "$pair3" "$pair4" "$three1" "$three2" "$three3" "$straight" "$four1" "$four2" | fmt -1 > debug.files.test
-
-## END DEBUG
-
-
-clean
