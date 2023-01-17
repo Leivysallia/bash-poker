e3035d31d2c35cffb09c8bd4fa900564ae6ca6f2
Leivysallia <aelenydd@gmail.com>
2023-01-17 15:38:25 -0500

    bug fix
    Fixed bug where straights with ace low were displaying as ace high

--- lambda/poker.sh
+++ lambda/poker.sh
@@ -8,83 +8,83 @@ freshstart() {
 
 vars() {
 
-    re='[1-5]'
-    unset flag
-    unset flush
-    unset repeat
-    unset discard
-    unset one
-    unset two
-    unset three
-    unset value
-    unset keep
-    unset "display[@]"
-    unset "library[@]"
-    unset "mchand[@]"
-    unset "iteration[@]"
-    unset "hand[@]"
-    unset disone
-    unset distwo
-    unset disthree
+	re='[1-5]'
+	unset flag
+	unset flush
+	unset repeat
+	unset discard
+	unset one
+	unset two
+	unset three
+	unset value
+	unset keep
+	unset "display[@]"
+	unset "library[@]"
+	unset "mchand[@]"
+	unset "iteration[@]"
+	unset "hand[@]"
+	unset disone
+	unset distwo
+	unset disthree
 
-    library[0]="1 of Spades"
-    library[1]="2 of Spades"
-    library[2]="3 of Spades"
-    library[3]="4 of Spades"
-    library[4]="5 of Spades"
-    library[5]="6 of Spades"
-    library[6]="7 of Spades"
-    library[7]="8 of Spades"
-    library[8]="9 of Spades"
-    library[9]="10 of Spades"
-    library[10]="11 of Spades"
-    library[11]="12 of Spades"
-    library[12]="13 of Spades"
-    library[13]="13 of Diamonds"
-    library[14]="12 of Diamonds"
-    library[15]="11 of Diamonds"
-    library[16]="10 of Diamonds"
-    library[17]="9 of Diamonds"
-    library[18]="8 of Diamonds"
-    library[19]="7 of Diamonds"
-    library[20]="6 of Diamonds"
-    library[21]="5 of Diamonds"
-    library[22]="4 of Diamonds"
-    library[23]="3 of Diamonds"
-    library[24]="2 of Diamonds"
-    library[25]="1 of Diamonds"
-    library[26]="1 of Clubs"
-    library[27]="2 of Clubs"
-    library[28]="3 of Clubs"
-    library[29]="4 of Clubs"
-    library[30]="5 of Clubs"
-    library[31]="6 of Clubs"
-    library[32]="7 of Clubs"
-    library[33]="8 of Clubs"
-    library[34]="9 of Clubs"
-    library[35]="10 of Clubs"
-    library[36]="11 of Clubs"
-    library[37]="12 of Clubs"
-    library[38]="13 of Clubs"
-    library[39]="13 of Hearts"
-    library[40]="12 of Hearts"
-    library[41]="11 of Hearts"
-    library[42]="10 of Hearts"
-    library[43]="9 of Hearts"
-    library[44]="8 of Hearts"
-    library[45]="7 of Hearts"
-    library[46]="6 of Hearts"
-    library[47]="5 of Hearts"
-    library[48]="4 of Hearts"
-    library[49]="3 of Hearts"
-    library[50]="2 of Hearts"
-    library[51]="1 of Hearts"
+	library[0]="1 of Spades"
+	library[1]="2 of Spades"
+	library[2]="3 of Spades"
+	library[3]="4 of Spades"
+	library[4]="5 of Spades"
+	library[5]="6 of Spades"
+	library[6]="7 of Spades"
+	library[7]="8 of Spades"
+	library[8]="9 of Spades"
+	library[9]="10 of Spades"
+	library[10]="11 of Spades"
+	library[11]="12 of Spades"
+	library[12]="13 of Spades"
+	library[13]="13 of Diamonds"
+	library[14]="12 of Diamonds"
+	library[15]="11 of Diamonds"
+	library[16]="10 of Diamonds"
+	library[17]="9 of Diamonds"
+	library[18]="8 of Diamonds"
+	library[19]="7 of Diamonds"
+	library[20]="6 of Diamonds"
+	library[21]="5 of Diamonds"
+	library[22]="4 of Diamonds"
+	library[23]="3 of Diamonds"
+	library[24]="2 of Diamonds"
+	library[25]="1 of Diamonds"
+	library[26]="1 of Clubs"
+	library[27]="2 of Clubs"
+	library[28]="3 of Clubs"
+	library[29]="4 of Clubs"
+	library[30]="5 of Clubs"
+	library[31]="6 of Clubs"
+	library[32]="7 of Clubs"
+	library[33]="8 of Clubs"
+	library[34]="9 of Clubs"
+	library[35]="10 of Clubs"
+	library[36]="11 of Clubs"
+	library[37]="12 of Clubs"
+	library[38]="13 of Clubs"
+	library[39]="13 of Hearts"
+	library[40]="12 of Hearts"
+	library[41]="11 of Hearts"
+	library[42]="10 of Hearts"
+	library[43]="9 of Hearts"
+	library[44]="8 of Hearts"
+	library[45]="7 of Hearts"
+	library[46]="6 of Hearts"
+	library[47]="5 of Hearts"
+	library[48]="4 of Hearts"
+	library[49]="3 of Hearts"
+	library[50]="2 of Hearts"
+	library[51]="1 of Hearts"
 
-    display[0]=":  "
-    display[1]=":  "
-    display[2]=":  "
-    display[3]=":  "
-    display[4]=":  "
+	display[0]=":  "
+	display[1]=":  "
+	display[2]=":  "
+	display[3]=":  "
+	display[4]=":  "
 
 }
 
@@ -594,6 +594,10 @@ iswin() {
 
 	if [[ "$straight" -eq 1 ]]; then
 		flag="Straight"
+		value="$num5"
+		if [[ $num2 -eq 10 ]]; then
+			value="$num1"
+		fi
 		display[3]=": +"
 		display[4]=": +"
 		display[0]=": +"
@@ -820,8 +824,6 @@ hold() {
 
 game() {
 
-	
-	
 	echo "-------------------------------"
 	read -n1 -r -p $'Play Again? [0/1] ' repeat
 
@@ -849,6 +851,12 @@ play() {
 
 	mull
 
+##	mchand[0]="${library[0]}"
+##	mchand[1]="${library[13]}"
+##	mchand[2]="${library[14]}"
+##	mchand[3]="${library[15]}"
+##	mchand[4]="${library[16]}"
+
 	ruffle
 	var
 	ifcalc
@@ -869,10 +877,8 @@ vars
 
 play
 
-while [[ $repeat -eq 1 ]]
-do
-	if [[ ! $repeat -eq 1 ]]
-	then
+while [[ $repeat -eq 1 ]]; do
+	if [[ ! $repeat -eq 1 ]]; then
 		return 1
 	fi
 	play
