bf35996296957abf3e21ee14d48d4ef8ddf596a4
Leivysallia <aelenydd@gmail.com>
2023-01-18 00:02:37 -0500

    rename folders

---
+++ lambda.beta/vars.bash
@@ -0,0 +1,319 @@
+#!/bin/bash
+
+acediamonds="┌┈┈┈┐
+|♦--|
+|-A-|
+|--♦|
+└┈┈┈┘"
+twodiamonds="┌┈┈┈┐
+|♦--|
+|-2-|
+|--♦|
+└┈┈┈┘"
+threediamonds="┌┈┈┈┐
+|♦--|
+|-3-|
+|--♦|
+└┈┈┈┘"
+fourdiamonds="┌┈┈┈┐
+|♦--|
+|-4-|
+|--♦|
+└┈┈┈┘"
+fivediamonds="┌┈┈┈┐
+|♦--|
+|-5-|
+|--♦|
+└┈┈┈┘"
+sixdiamonds="┌┈┈┈┐
+|♦--|
+|-6-|
+|--♦|
+└┈┈┈┘"
+sevendiamonds="┌┈┈┈┐
+|♦--|
+|-7-|
+|--♦|
+└┈┈┈┘"
+eightdiamonds="┌┈┈┈┐
+|♦--|
+|-8-|
+|--♦|
+└┈┈┈┘"
+ninediamonds="┌┈┈┈┐
+|♦--|
+|-9-|
+|--♦|
+└┈┈┈┘"
+tendiamonds="┌┈┈┈┐
+|♦--|
+|-X-|
+|--♦|
+└┈┈┈┘"
+jackdiamonds="┌┈┈┈┐
+|♦--|
+|-J-|
+|--♦|
+└┈┈┈┘"
+queendiamonds="┌┈┈┈┐
+|♦--|
+|-Q-|
+|--♦|
+└┈┈┈┘"
+kingdiamonds="┌┈┈┈┐
+|♦--|
+|-K-|
+|--♦|
+└┈┈┈┘"
+acehearts="┌┈┈┈┐
+|♥--|
+|-A-|
+|--♥|
+└┈┈┈┘"
+twohearts="┌┈┈┈┐
+|♥--|
+|-2-|
+|--♥|
+└┈┈┈┘"
+threehearts="┌┈┈┈┐
+|♥--|
+|-3-|
+|--♥|
+└┈┈┈┘"
+fourhearts="┌┈┈┈┐
+|♥--|
+|-4-|
+|--♥|
+└┈┈┈┘"
+fivehearts="┌┈┈┈┐
+|♥--|
+|-5-|
+|--♥|
+└┈┈┈┘"
+sixhearts="┌┈┈┈┐
+|♥--|
+|-6-|
+|--♥|
+└┈┈┈┘"
+sevenhearts="┌┈┈┈┐
+|♥--|
+|-7-|
+|--♥|
+└┈┈┈┘"
+eighthearts="┌┈┈┈┐
+|♥--|
+|-8-|
+|--♥|
+└┈┈┈┘"
+ninehearts="┌┈┈┈┐
+|♥--|
+|-9-|
+|--♥|
+└┈┈┈┘"
+tenhearts="┌┈┈┈┐
+|♥--|
+|-X-|
+|--♥|
+└┈┈┈┘"
+jackhearts="┌┈┈┈┐
+|♥--|
+|-J-|
+|--♥|
+└┈┈┈┘"
+queenhearts="┌┈┈┈┐
+|♥--|
+|-Q-|
+|--♥|
+└┈┈┈┘"
+kinghearts="┌┈┈┈┐
+|♥--|
+|-K-|
+|--♥|
+└┈┈┈┘"
+acespades="┌┈┈┈┐
+|♠--|
+|-A-|
+|--♠|
+└┈┈┈┘"
+twospades="┌┈┈┈┐
+|♠--|
+|-2-|
+|--♠|
+└┈┈┈┘"
+threespades="┌┈┈┈┐
+|♠--|
+|-3-|
+|--♠|
+└┈┈┈┘"
+fourspades="┌┈┈┈┐
+|♠--|
+|-4-|
+|--♠|
+└┈┈┈┘"
+fivespades="┌┈┈┈┐
+|♠--|
+|-5-|
+|--♠|
+└┈┈┈┘"
+sixspades="┌┈┈┈┐
+|♠--|
+|-6-|
+|--♠|
+└┈┈┈┘"
+sevenspades="┌┈┈┈┐
+|♠--|
+|-7-|
+|--♠|
+└┈┈┈┘"
+eightspades="┌┈┈┈┐
+|♠--|
+|-8-|
+|--♠|
+└┈┈┈┘"
+ninespades="┌┈┈┈┐
+|♠--|
+|-9-|
+|--♠|
+└┈┈┈┘"
+tenspades="┌┈┈┈┐
+|♠--|
+|-X-|
+|--♠|
+└┈┈┈┘"
+jackspades="┌┈┈┈┐
+|♠--|
+|-J-|
+|--♠|
+└┈┈┈┘"
+queenspades="┌┈┈┈┐
+|♠--|
+|-Q-|
+|--♠|
+└┈┈┈┘"
+kingspades="┌┈┈┈┐
+|♠--|
+|-K-|
+|--♠|
+└┈┈┈┘"
+aceclubs="┌┈┈┈┐
+|♣--|
+|-A-|
+|--♣|
+└┈┈┈┘"
+twoclubs="┌┈┈┈┐
+|♣--|
+|-2-|
+|--♣|
+└┈┈┈┘"
+threeclubs="┌┈┈┈┐
+|♣--|
+|-3-|
+|--♣|
+└┈┈┈┘"
+fourclubs="┌┈┈┈┐
+|♣--|
+|-4-|
+|--♣|
+└┈┈┈┘"
+fiveclubs="┌┈┈┈┐
+|♣--|
+|-5-|
+|--♣|
+└┈┈┈┘"
+sixclubs="┌┈┈┈┐
+|♣--|
+|-6-|
+|--♣|
+└┈┈┈┘"
+sevenclubs="┌┈┈┈┐
+|♣--|
+|-7-|
+|--♣|
+└┈┈┈┘"
+eightclubs="┌┈┈┈┐
+|♣--|
+|-8-|
+|--♣|
+└┈┈┈┘"
+nineclubs="┌┈┈┈┐
+|♣--|
+|-9-|
+|--♣|
+└┈┈┈┘"
+tenclubs="┌┈┈┈┐
+|♣--|
+|-X-|
+|--♣|
+└┈┈┈┘"
+jackclubs="┌┈┈┈┐
+|♣--|
+|-J-|
+|--♣|
+└┈┈┈┘"
+queenclubs="┌┈┈┈┐
+|♣--|
+|-Q-|
+|--♣|
+└┈┈┈┘"
+kingclubs="┌┈┈┈┐
+|♣--|
+|-K-|
+|--♣|
+└┈┈┈┘"
+
+acediamonds=$(echo -e "\033[1;31m$acediamonds\033[0m")
+twodiamonds=$(echo -e "\033[1;31m$twodiamonds\033[0m")
+threediamonds=$(echo -e "\033[1;31m$threediamonds\033[0m")
+fourdiamonds=$(echo -e "\033[1;31m$fourdiamonds\033[0m")
+fivediamonds=$(echo -e "\033[1;31m$fivediamonds\033[0m")
+sixdiamonds=$(echo -e "\033[1;31m$sixdiamonds\033[0m")
+sevendiamonds=$(echo -e "\033[1;31m$sevendiamonds\033[0m")
+eightdiamonds=$(echo -e "\033[1;31m$eightdiamonds\033[0m")
+ninediamonds=$(echo -e "\033[1;31m$ninediamonds\033[0m")
+tendiamonds=$(echo -e "\033[1;31m$tendiamonds\033[0m")
+jackdiamonds=$(echo -e "\033[1;31m$jackdiamonds\033[0m")
+queendiamonds=$(echo -e "\033[1;31m$queendiamonds\033[0m")
+kingdiamonds=$(echo -e "\033[1;31m$kingdiamonds\033[0m")
+acehearts=$(echo -e "\033[1;31m$acehearts\033[0m")
+twohearts=$(echo -e "\033[1;31m$twohearts\033[0m")
+threehearts=$(echo -e "\033[1;31m$threehearts\033[0m")
+fourhearts=$(echo -e "\033[1;31m$fourhearts\033[0m")
+fivehearts=$(echo -e "\033[1;31m$fivehearts\033[0m")
+sixhearts=$(echo -e "\033[1;31m$sixhearts\033[0m")
+sevenhearts=$(echo -e "\033[1;31m$sevenhearts\033[0m")
+eighthearts=$(echo -e "\033[1;31m$eighthearts\033[0m")
+ninehearts=$(echo -e "\033[1;31m$ninehearts\033[0m")
+tenhearts=$(echo -e "\033[1;31m$tenhearts\033[0m")
+jackhearts=$(echo -e "\033[1;31m$jackhearts\033[0m")
+queenhearts=$(echo -e "\033[1;31m$queenhearts\033[0m")
+kinghearts=$(echo -e "\033[1;31m$kinghearts\033[0m")
+aceclubs=$(echo -e "\033[0;36m$aceclubs\033[0m")
+twoclubs=$(echo -e "\033[0;36m$twoclubs\033[0m")
+threeclubs=$(echo -e "\033[0;36m$threeclubs\033[0m")
+fourclubs=$(echo -e "\033[0;36m$fourclubs\033[0m")
+fiveclubs=$(echo -e "\033[0;36m$fiveclubs\033[0m")
+sixclubs=$(echo -e "\033[0;36m$sixclubs\033[0m")
+sevenclubs=$(echo -e "\033[0;36m$sevenclubs\033[0m")
+eightclubs=$(echo -e "\033[0;36m$eightclubs\033[0m")
+nineclubs=$(echo -e "\033[0;36m$nineclubs\033[0m")
+tenclubs=$(echo -e "\033[0;36m$tenclubs\033[0m")
+jackclubs=$(echo -e "\033[0;36m$jackclubs\033[0m")
+queenclubs=$(echo -e "\033[0;36m$queenclubs\033[0m")
+kingclubs=$(echo -e "\033[0;36m$kingclubs\033[0m")
+acespades=$(echo -e "\033[0;36m$acespades\033[0m")
+twospades=$(echo -e "\033[0;36m$twospades\033[0m")
+threespades=$(echo -e "\033[0;36m$threespades\033[0m")
+fourspades=$(echo -e "\033[0;36m$fourspades\033[0m")
+fivespades=$(echo -e "\033[0;36m$fivespades\033[0m")
+sixspades=$(echo -e "\033[0;36m$sixspades\033[0m")
+sevenspades=$(echo -e "\033[0;36m$sevenspades\033[0m")
+eightspades=$(echo -e "\033[0;36m$eightspades\033[0m")
+ninespades=$(echo -e "\033[0;36m$ninespades\033[0m")
+tenspades=$(echo -e "\033[0;36m$tenspades\033[0m")
+jackspades=$(echo -e "\033[0;36m$jackspades\033[0m")
+queenspades=$(echo -e "\033[0;36m$queenspades\033[0m")
+kingspades=$(echo -e "\033[0;36m$kingspades\033[0m")
+
+
+
+paste <(printf %s "$acehearts") <(printf %s "$twohearts")
